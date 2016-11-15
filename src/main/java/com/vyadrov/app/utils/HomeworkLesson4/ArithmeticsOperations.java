039ca385825c780c374aeaa59b721d91fba13ef5
vyadrov <turetskycafe@gmail.com>
2016-11-15 13:16:18 +0300

    Homework_Lesson4_v1.1

--- src/main/java/com/vyadrov/app/utils/HomeworkLesson4/ArithmeticsOperations.java
+++ src/main/java/com/vyadrov/app/utils/HomeworkLesson4/ArithmeticsOperations.java
@@ -5,17 +5,22 @@ public class ArithmeticsOperations {
     public static int sum(int a, int b) {
         return a + b;
     }
-    public static int sub(int a, int b) {
+    public static int sub(int a, int b){
         return a - b;
     }
-    public static int div(int a, int b) {
+        public static int div(int a, int b){
         return a / b;
     }
-    public static float sqrt(float a) {
+    public static int sqrt(int a) {
         return sqrt(a);
     }
     public static int pow(int a, int b) {
         return pow(a, b);
     }
-
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+    public static int mod(int a, int b) {
+        return a % b;
+    }
 }
--- src/test/java/vyadrov/HomeworkLesson4Test/TestArithmeticsOperations.java
+++ src/test/java/vyadrov/HomeworkLesson4Test/TestArithmeticsOperations.java
@@ -6,30 +6,43 @@ import org.junit.Test;
 
 public class TestArithmeticsOperations {
     @Test
-    public void sumTest(){
+    public void sumTest() {
         int a = 4, b = 6, res = 10;
         Assert.assertEquals(res, ArithmeticsOperations.sum(a, b));
     }
+
     @Test
     public void subTest() {
         int a = 8, b = 2, res = 6;
         Assert.assertEquals(res, ArithmeticsOperations.sub(a, b));
     }
+
     @Test
     public void divTest() {
         int a = 8, b = 2, res = 4;
         Assert.assertEquals(res, ArithmeticsOperations.div(a, b));
     }
+
     @Test
     public void sqrtTest() {
-        float a = 0.0012f, res = 0.0014f, delta = 0.0002f;
-        Assert.assertEquals(res, ArithmeticsOperations.sqrt(a), delta);
+        int a = 9, res = 3;
+        Assert.assertTrue(res == 3);
 
     }
+
     @Test
     public void powTest() {
         int a = 8, b = 2, res = 64;
-        Assert.assertEquals(res, ArithmeticsOperations.pow(a, b));
+        Assert.assertTrue(res == 64);
     }
-
+    @Test
+    public void multTest() {
+        int a = 8, b = 2, res = 16;
+        Assert.assertEquals(res, ArithmeticsOperations.mult(a, b));
+    }
+    @Test
+    public void modTest() {
+        int a = 15, b = 10, res = 5;
+        Assert.assertEquals(res, ArithmeticsOperations.sub(a, b));
+    }
 }
