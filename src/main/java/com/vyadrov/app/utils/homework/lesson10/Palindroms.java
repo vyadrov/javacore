99be9414f6b17df25a018cb2a2d39693be942bd7
vyadrov <turetskycafe@gmail.com>
2016-12-09 10:21:27 +0200

    Homework_10_v1.2

--- src/main/java/com/vyadrov/app/utils/homework/lesson10/Palindroms.java
+++ src/main/java/com/vyadrov/app/utils/homework/lesson10/Palindroms.java
@@ -3,7 +3,7 @@ package com.vyadrov.app.utils.homework.lesson10;
 import java.util.Scanner;
 
 public class Palindroms {
-    public static void polindromByLoopFor() {
+    public static void palindromeFirstMethod() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter your word:");
         String word = scanner.nextLine();
@@ -12,17 +12,35 @@ public class Palindroms {
             reverse = reverse + word.charAt(i);
         }
         if (reverse.equalsIgnoreCase(word)) {
-            System.out.println("Your world is Polindrome");
+            System.out.println("Your world is Palindrome");
         } else {
-            System.out.println("Your world is NOT Polindrome");
+            System.out.println("Your world is NOT Palindrome");
         }
     }
-    public static void polindromBoolean() {
+    public static void palindromeSecondMethod() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter your word:");
         String word = scanner.nextLine();
         StringBuffer buffer = new StringBuffer(word);
         buffer.reverse();
-        System.out.println(buffer.toString().equals(word));
+        buffer.toString();
+        if (buffer.toString().equals(word) == true) {
+            System.out.println("Your world is Palindrome");
+        } else {
+            System.out.println("Your world is NOT Palindrome");
+        }
+    }
+    public static boolean palindromeThirdMethod() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter your word:");
+        String word = scanner.nextLine();
+        for (int i = 0, j = word.length() - 1; i <= j; i++, j--) {
+            if (word.charAt(i) == word.charAt(j)) {
+                System.out.println("Your world is Palindrome");
+                return true;
+            }
+        }
+        System.out.println("Your world is NOT Palindrome");
+        return false;
     }
 }
--- src/main/java/com/vyadrov/runners/homework/lesson10/Lesson10Runner.java
+++ src/main/java/com/vyadrov/runners/homework/lesson10/Lesson10Runner.java
@@ -2,9 +2,32 @@ package com.vyadrov.runners.homework.lesson10;
 
 import com.vyadrov.app.utils.homework.lesson10.Palindroms;
 
+import java.util.Scanner;
+
 public class Lesson10Runner {
     public static void main(String[] args) {
-        //Palindroms.polindromByLoopFor();
-        Palindroms.polindromBoolean();
+        int number;
+        do {
+            System.out.println("Hi! Please, enter the number of Method which will determine String is palindrome OR Not: " + "\n" +
+                    "1. First Method." + "\n" +
+                    "2. Second Method." + "\n" +
+                    "3. Third Method." + "\n" +
+                    "0. Exit.\n");
+            Scanner scanner = new Scanner(System.in);
+            int numberOfTask = scanner.nextInt();
+            switch (numberOfTask) {
+                case 1:
+                    Palindroms.palindromeFirstMethod();
+                    break;
+                case 2:
+                    Palindroms.palindromeSecondMethod();
+                    break;
+                case 3:
+                    Palindroms.palindromeThirdMethod();
+                    break;
+                default:
+                    System.out.println("You have made incorrect choice.");
+            } number = numberOfTask;
+        } while (number !=0);
     }
 }
