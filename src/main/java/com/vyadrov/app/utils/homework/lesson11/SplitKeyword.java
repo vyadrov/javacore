c9a4337007bf1009cafd17e3b88cb5c4d5efa4c4
vyadrov <turetskycafe@gmail.com>
2016-12-13 10:01:35 +0200

    Homework11_v1.2_Task_C

--- src/main/java/com/vyadrov/app/utils/homework/lesson11/SplitKeyword.java
+++ src/main/java/com/vyadrov/app/utils/homework/lesson11/SplitKeyword.java
@@ -3,19 +3,18 @@ package com.vyadrov.app.utils.homework.lesson11;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Created by Виталий on 12.12.2016.
- */
+
 public class SplitKeyword {
     public static void spliKeyword() {
-        System.out.println("Please, enter symbol which will be a spliter:");
+        System.out.println("Please, enter KEYWORD which will be a spliter:");
         Scanner scanner1 = new Scanner(System.in);
-        String inputSpliter = scanner1.nextLine();
+        String keywordInputSpliter = scanner1.nextLine();
         System.out.println("Enter your string:");
         Scanner scanner2 = new Scanner(System.in);
         String inputString = scanner2.nextLine();
-        String[] splittedStringArray = inputString.split(inputSpliter);
-        System.out.println(Arrays.toString(splittedStringArray));
-
+        String[] splittedStringArray = inputString.split(keywordInputSpliter);
+        for (int i = 0; i < splittedStringArray.length; i++) {
+            System.out.println(splittedStringArray[i].trim());
+        }
     }
 }
