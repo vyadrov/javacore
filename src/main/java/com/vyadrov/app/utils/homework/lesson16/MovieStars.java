5b59230bcac078a9f032f79b71b22eabc99a5bba
vyadrov <turetskycafe@gmail.com>
2017-01-16 21:23:10 +0200

    Homework16v1.4

--- src/main/java/com/vyadrov/app/utils/homework/lesson16/MovieStars.java
+++ src/main/java/com/vyadrov/app/utils/homework/lesson16/MovieStars.java
@@ -5,6 +5,11 @@ public enum MovieStars {
     AL_PACINO("Al Pacino", 76),
     ROBERT_DE_NIRO("Robert De Niro", 73),
     LEONARDO_DICAPRIO("Leonardo DiCaprio", 42),
+    BEN_AFFLECK("Ben Affleck", 44),
+    BRUCE_WILLIS("Bruce Willis", 61),
+    JOHN_MALKOVICH("John Malkovich", 63),
+    ANTHONY_HOPKINS("Anthony Hopkins", 79),
+    MARLON_BRANDO("Marlon Brando", 80),
     TOM_HARDY("Tom Hardy", 39);
 
     private String actor;
--- src/main/java/com/vyadrov/runners/homework/lesson16/Lesson16Runner.java
+++ src/main/java/com/vyadrov/runners/homework/lesson16/Lesson16Runner.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson16Runner {
-    public static boolean[] temp;
+    public static boolean[] movieStarIdIsRepeated;
     public static int i = 0;
     public static void main(String[] args) {
         int inputAge;
@@ -15,31 +15,27 @@ public class Lesson16Runner {
         String randomStar = "";
         MovieStars[] starsArray = MovieStars.values();
         int a = starsArray.length;
-        temp = new boolean[a];
-        for (boolean k: temp) {
-            int i = 0;
-            temp[i] = false;
+        movieStarIdIsRepeated = new boolean[a];
+        for (int i = 0; i < movieStarIdIsRepeated.length; i++) {
+            movieStarIdIsRepeated[i] = false;
         }
-       // i++;
         Random random = new Random();
         try(Scanner scanner = new Scanner(System.in)) {
             do {
                 randomNumberMovieStar = random.nextInt(starsArray.length);
-                if (temp[i] == true) {
+                if (movieStarIdIsRepeated[randomNumberMovieStar] == true) {
                     random.nextInt(starsArray.length);
                 } else {
-                    temp[i] = true;
+                    movieStarIdIsRepeated[randomNumberMovieStar] = true;
                     randomStar = starsArray[randomNumberMovieStar].getActor();
                     System.out.println("Please enter how old is " + randomStar + " do you think?");
                     inputAge = scanner.nextInt();
                     if (inputAge == starsArray[randomNumberMovieStar].getYearsOld()) {
                         System.out.println("Congrats! you are right.");
                         guessedAttemps++;
-                        i++;
                     } else {
                         System.out.println("Sorry, but you are wrong! His age is " + starsArray[randomNumberMovieStar].getYearsOld() + ". Try again with another actor:");
-                        temp[i] = true;
-                        i++;
+                        movieStarIdIsRepeated[randomNumberMovieStar] = true;
                     }
                 }
             } while (guessedAttemps !=3);
