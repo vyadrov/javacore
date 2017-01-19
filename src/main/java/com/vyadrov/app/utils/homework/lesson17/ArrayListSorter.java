fce845e3dfd92f02f27e708618c9bde694e35602
vyadrov <turetskycafe@gmail.com>
2017-01-19 22:26:19 +0200

    Homeworkv17_v1.2

--- src/main/java/com/vyadrov/app/utils/homework/lesson17/ArrayListSorter.java
+++ src/main/java/com/vyadrov/app/utils/homework/lesson17/ArrayListSorter.java
@@ -4,20 +4,17 @@ import java.util.TreeSet;
 
 public class ArrayListSorter {
     public static void main(String[] args) {
-        TreeSet<String> states = new TreeSet<>();
+        System.out.println("Please, enter any words: ");
+        TreeSet<String> words = new TreeSet<>();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println("Please, enter states which you know: ");
+        String input;
         do {
-            states.add(input);
-            //System.out.println(states);
+            input = scanner.nextLine();
+            words.add(input);
         } while (input.length() != 0);
-        System.out.println(states);
-
-
-        /*for(String elements: states) {
+        System.out.print("You entered this unique words (Without Dublicates!):");
+        for(String elements: words) {
             System.out.println(elements);
-        }*/
-
+        }
     }
 }
