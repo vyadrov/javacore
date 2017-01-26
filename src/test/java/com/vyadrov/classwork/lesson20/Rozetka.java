f8e09f9aed4cf24b47adbd62cbfd5d0c945a7b87
vyadrov <turetskycafe@gmail.com>
2017-01-26 18:07:20 +0200

    Classwork20_v1.1

--- src/test/java/com/vyadrov/classwork/lesson20/Rozetka.java
+++ src/test/java/com/vyadrov/classwork/lesson20/Rozetka.java
@@ -4,32 +4,45 @@ package com.vyadrov.classwork.lesson20;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Rozetka {
 
-    private FirefoxDriver webdriver;
+    private FirefoxDriver driver;
     private String baseUrl = "http://rozetka.com.ua/";
+    WebElement webElement;
 
     @Before
     public void setup () throws Exception {
-        webdriver = new FirefoxDriver();
-        webdriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
+   /* @Test
+    public void testCheckTitle() {
+        driver.get(baseUrl);
+        driver.findElementByXPath("./*//*[@id='head_banner_container']/div/div[3]/div/div/div[1]/div[1]/div[1]/ul/li[2]/a");
+        driver.findElementByLinkText("Корзина");
+    }*/
     @Test
-    public void testCHeckTitle() {
-        webdriver.get(baseUrl);
-        webdriver.findElementByXPath(".//*[@id='body-header']/div[3]/div/div[2]/div[1]/img");
-        //webdriver.findElementByLinkText("Интернет магазин Rozetka.ua");
+    public void testSearch() {
+        driver.get(baseUrl);
+        webElement = driver.findElementByCssSelector(".rz-header-search-input-text.passive");
+        webElement.click();
+        webElement.sendKeys("iphone 6s");
+        webElement = driver.findElementByName("2");
+        //webElement = driver.findElementByLinkText("iphone 6s");
+        webElement.click();
+
     }
 
-    @After
+    /*@After
     public void quit() {
-        webdriver.quit();
-    }
+        driver.quit();
+    }*/
 
 
 }
