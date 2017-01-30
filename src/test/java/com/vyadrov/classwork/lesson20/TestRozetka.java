82e07a028979698fea6ce381b9e864cb669f1f4a
vyadrov <turetskycafe@gmail.com>
2017-01-30 14:31:21 +0200

    Classwork20.v1.3

--- src/test/java/com/vyadrov/classwork/lesson20/TestRozetka.java
+++ src/test/java/com/vyadrov/classwork/lesson20/TestRozetka.java
@@ -1,6 +1,6 @@
 package com.vyadrov.classwork.lesson20;
 
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
@@ -8,8 +8,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class Rozetka {
-
+/**
+ * Created by vyadrov on 1/30/17.
+ */
+public class TestRozetka {
     private FirefoxDriver driver;
     private String baseUrl = "http://rozetka.com.ua/";
     //private String baseUrl2 = "http://www.fast-torrent.ru/";
@@ -18,31 +20,27 @@ public class Rozetka {
     @Before
     public void setup () throws Exception {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS.SECONDS);
     }
-
-   /* @Test
+    @Test
     public void testCheckTitle() {
         driver.get(baseUrl);
-        driver.findElementByXPath("./*//*[@id='head_banner_container']/div/div[3]/div/div/div[1]/div[1]/div[1]/ul/li[2]/a");
-        driver.findElementByLinkText("Корзина");
-    }*/
+        driver.findElementByCssSelector(".logo>img");
+    }
     @Test
     public void testSearch() {
         driver.get(baseUrl);
         webElement = driver.findElementByCssSelector(".rz-header-search-input-text.passive");
         webElement.click();
-        webElement.sendKeys("iphone 6s");
-        webElement = driver.findElementByName("2");
-        //webElement = driver.findElementByLinkText("iphone 6s");
+        webElement.sendKeys("iphone 6s plus");
+        webElement = driver.findElementByLinkText("iphone 6s plus 128gb");
         webElement.click();
+
 
     }
 
-    /*@After
+    @After
     public void quit() {
         driver.quit();
-    }*/
-
-
+    }
 }
